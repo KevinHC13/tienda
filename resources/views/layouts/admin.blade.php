1090ffcc8ff5719b6573f5fc1114015874281acf
Kevin Hernandez <kevinale1231@gmail.com>
2023-07-13 23:39:33 -0600

    Modificando sidbar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -302,12 +302,6 @@
               <span class="ml-2 text-sm tracking-wide truncate">Clientes</span>
             </a>
           </li>
-
-          <li class="px-5 hidden md:block">
-            <div class="flex flex-row items-center mt-5 h-8">
-              <div class="text-sm font-light tracking-wide text-gray-400 uppercase">Settings</div>
-            </div>
-          </li>
           <li>
             <a href="{{ route('provider.index') }}" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
               <span class="inline-flex justify-center items-center ml-4">
